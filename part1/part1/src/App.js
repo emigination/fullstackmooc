72c612b0fe5c22b3b740f6ded6c52b271c58511e
Emilia Andersson <emilia.andersson@helsinki.fi>
2021-06-16 09:32:45 +0300

    Tehtävä 1.2

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
       <p>
@@ -17,6 +17,16 @@ const Content = (props) => {
     </div>
   )
 }
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+ }
 
 const Footer = (props) => {
   return (
@@ -38,9 +48,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Footer exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
