684957f2806da2545e5efa4405ba7e22ae0dadc5
Emilia Andersson <emilia.andersson@helsinki.fi>
2021-11-05 21:27:33 +0200

    Tehtävät 1.9-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,21 +4,39 @@ const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}> {text} </button>
 )
 
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
+
+  if (total > 0) {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="All" value={total} />
+          <StatisticLine text="Average" value={(good - bad) / total} />
+          <StatisticLine text="Positive" value={good / total * 100 + '%'} />
+        </tbody>
+    </table>
+    )
+  }
 
   return (
     <div>
       <p>
-        Good {good}<br />
-        Neutral {neutral}<br />
-        Bad {bad}<br />
-        All {total}<br />
-        Average {(good - bad) / total}<br />
-        Positive {good / total * 100} %<br />
+        No feedback given
       </p>
     </div>
   )
+
 }
 
 const App = () => {
@@ -33,7 +51,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
       <h1>Statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
