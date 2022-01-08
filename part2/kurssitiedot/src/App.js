95886b011f96fc1643d7a2ef25749fa4ea5d6b19
Emilia Andersson <emilia.andersson@helsinki.fi>
2022-01-08 15:44:46 +0200

    Tehtävä 2.5

--- part2/kurssitiedot/src/App.js
+++ part2/kurssitiedot/src/App.js
@@ -1,49 +1,5 @@
 import React from 'react'
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-        {props.parts.map(part => <Part part={part} key={part.id}/>)}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const sum = props.parts.reduce((acc, cur) => acc + cur.exercises, 0)
-  return (
-    <div>
-      <p><b>Total of exercises {sum}</b></p>
-    </div>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header name={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
