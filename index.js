7fd91ee130ebdab1c098f4ec0dd1087e9db1888f
Emilia Andersson <emilia.andersson@helsinki.fi>
2022-02-17 20:37:48 +0200

    Tehtävä 3.8

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const app = express()
 var morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('data', function (req, res) { return JSON.stringify(req.body)})
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.data(req, res)
+  ].join(' ')
+}))
 
 let contacts = [
   {
