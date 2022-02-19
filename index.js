42fc5d4c3ec0ce2dcbe7dfe94f6a0215672a52a3
Emilia Andersson <emilia.andersson@helsinki.fi>
2022-02-19 19:06:17 +0200

    Tehtävä 3.11

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 var morgan = require('morgan')
 
-
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 morgan.token('data', function (req, res) { return JSON.stringify(req.body)})
