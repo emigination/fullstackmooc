ff3f9628fc66c4aa7175b3d82d14e2b19ac6b5d7
Emilia Andersson <emilia.andersson@helsinki.fi>
2022-02-24 15:19:41 +0200

    Tehtävät 3.13-3.14

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 var morgan = require('morgan')
+const Contact = require('./models/contact')
+const { response } = require('express')
+const PORT = process.env.PORT
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-morgan.token('data', function (req, res) { return JSON.stringify(req.body)})
+morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan((tokens, req, res) => {
   return [
     tokens.method(req, res),
@@ -18,31 +22,33 @@ app.use(morgan((tokens, req, res) => {
   ].join(' ')
 }))
 
-let contacts = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
+// let contacts = [
+//   {
+//     "name": "Arto Hellas",
+//     "number": "040-123456",
+//     "id": 1
+//   },
+//   {
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523",
+//     "id": 2
+//   },
+//   {
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345",
+//     "id": 3
+//   },
+//   {
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122",
+//     "id": 4
+//   }
+// ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(contacts)
+  Contact.find({}).then(result => {
+    res.json(result)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -50,13 +56,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  const person = contacts.find(contact => contact.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Contact.findById(req.params.id).then(contact => {
+    res.json(contact)
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -67,36 +69,25 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-
   if (!body.name) {
     return res.status(400).json({
       error: 'Name missing'
     })
   }
-
-  if (contacts.some((person) => person.name === body.name)) {
-    return res.status(400).json({
-      error: 'Name must be unique'
-    })
-  }
-
   if (!body.number) {
     return res.status(400).json({
       error: 'Number missing'
     })
   }
-
-  const contact = {
+  const contact = new Contact({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random()*1000000)
-  }
-
-  contacts = contacts.concat(contact)
-
-  res.json(contact)
+  })
+  contact.save().then(result => {
+    res.json(contact)
+    mongoose.connection.close()
+  })
 })
 
-const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
