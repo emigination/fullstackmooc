f1c5fe556599368dc6ecdb1e85545bff6fbe269f
Emilia Andersson <emilia.andersson@helsinki.fi>
2022-03-20 15:05:33 +0200

    Tehtävä 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,23 @@
-const dummy = (blogs) => 1
-
 const totalLikes = (blogs) => {
-  return blogs.map(blog => blog.likes).reduce(((sum, current) => sum + current))
+  return blogs.map(blog => blog.likes).reduce((sum, current) => sum + current)
+}
+
+const favoriteBlog = (blogs) => {
+  let max = 0
+  let fav = blogs[0]
+  for (const blog of blogs) {
+    if (blog.likes > max) {
+      max = blog.likes
+      fav = blog
+    }
+  }
+  return {
+    title: fav.title,
+    author: fav.author,
+    likes: max
+  }
 }
 
 module.exports = {
-  dummy, totalLikes
+  totalLikes, favoriteBlog
 }
