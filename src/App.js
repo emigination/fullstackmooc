fd6026d0c1b39e535937930c1f8b0b7b06e19f3d
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-07-29 18:58:32 +0300

    Tehtävä 5.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import { useState, useEffect } from 'react'
+import loginService from './services/login'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username:
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password:
+              <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
+      Logged in as "{user.name}"
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
