167d4d2dad0013f00c8877c4fe7622419f353e6e
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-07-30 17:38:12 +0300

    Tehtävä 5.3

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('user')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -27,13 +31,12 @@ const App = () => {
     event.preventDefault()
 
     try {
-      const user = await loginService.login({
-        username, password,
-      })
-      setUser(user)
+      const userLoggingIn = await loginService.login({ username, password })
+      setUser(userLoggingIn)
+      blogService.setToken(userLoggingIn.token)
       setUsername('')
       setPassword('')
-      window.localStorage.setItem('user', JSON.stringify(user))
+      window.localStorage.setItem('user', JSON.stringify(userLoggingIn))
     } catch (exception) {
       console.log(exception)
     }
@@ -42,6 +45,21 @@ const App = () => {
   const handleLogout = () => {
     window.localStorage.removeItem('user')
     setUser(null)
+    blogService.setToken(null)
+  }
+
+  const createNewBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newBlog = await blogService.createNew({ title, author, url })
+      setBlogs(blogs.concat(newBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exception) {
+      console.log(exception)
+    }
   }
 
   if (user === null) {
@@ -63,6 +81,7 @@ const App = () => {
               <input
               type="password"
               value={password}
+
               name="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
@@ -81,6 +100,37 @@ const App = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      <h2>create new</h2>
+      <form onSubmit={createNewBlog}>
+        <div>
+          title:
+            <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+            <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+            <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,10 +1,21 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
+const createNew = async blogData => {
+  const response = await axios.post(baseUrl, blogData, { headers: { Authorization: token } })
+  return response.data
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+export default { setToken, getAll, createNew }
