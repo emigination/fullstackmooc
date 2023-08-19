9301d624c8534de2512b8e159684ff26c185ff93
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-08-19 15:35:46 +0300

    Tehtävä 5.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,19 +2,19 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-blogsRouter.get('/', async(request, response) => {
+blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
-blogsRouter.post('/', async(request, response) => {
+blogsRouter.post('/', async (request, response) => {
   if (!request.user) {
     return response.status(401).json({ error: 'token missing' })
   }
 
   const blogParams = request.body
   if (!blogParams.title || !blogParams.url) {
-    return response.status(400).end();
+    return response.status(400).end()
   }
 
   if (!blogParams.likes) {
@@ -23,18 +23,19 @@ blogsRouter.post('/', async(request, response) => {
   const user = await User.findById(request.user)
   blogParams.user = user._id
   const blog = new Blog(blogParams)
-  await blog.save()
+  const savedBlog = await blog.save()
+  savedBlog.populate('user', { username: 1, name: 1 })
   user.blogs = user.blogs.concat(blog._id)
   await user.save()
-  response.status(201).json(blog)
+  response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async(request, response) => {
+blogsRouter.delete('/:id', async (request, response) => {
   if (!request.user) {
     return response.status(401).json({ error: 'token missing' })
   }
 
-  const blog = await Blog.findOne({_id: request.params.id, user: request.user})
+  const blog = await Blog.findOne({ _id: request.params.id, user: request.user })
   if (blog) {
     blog.deleteOne()
     response.status(204).end()
@@ -43,7 +44,7 @@ blogsRouter.delete('/:id', async(request, response) => {
   }
 })
 
-blogsRouter.put('/:id', async(request, response) => {
+blogsRouter.put('/:id', async (request, response) => {
   if (!request.user) {
     return response.status(401).json({ error: 'token missing' })
   }
