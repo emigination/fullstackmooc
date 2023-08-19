9eb2426b962227bb35c1c4241ab53d7689d85833
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-08-19 16:50:31 +0300

    Tehtävä 5.10

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,7 +4,7 @@ const BlogList = ({ blogs }) => {
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
