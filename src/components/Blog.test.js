d9a6454a435c7654fec9fab2d9ce37f810302f16
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-08-27 11:47:10 +0300

    Tehtävä 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title', () => {
   const blog = { title: 'Blog Title', url: 'https://example.com', likes: 0, user: { name: 'Blog User' } }
+
   render(<Blog blog={blog} isOwn={false} />)
+
   screen.getByText('Blog Title')
+})
+
+test('display blog details when view button is clicked', async () => {
+  const blog = { title: 'Blog Title', author: 'Blog Author', url: 'https://example.com', likes: 0, user: { name: 'Blog User' } }
+  render(<Blog blog={blog} isOwn={false} />)
+  const user = userEvent.setup()
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  screen.getByText('Blog Title Blog Author')
+  screen.getByText('https://example.com')
+  screen.getByText('0')
+  screen.getByText('Blog User')
 })
