15980bfb58e04c44bfce2899c445559d1f44b054
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-12-22 11:24:30 +0200

    Tehtävä 6.9

--- redux-anecdotes-main/src/components/AnecdoteList.jsx
+++ redux-anecdotes-main/src/components/AnecdoteList.jsx
@@ -1,8 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux'
+import Filter from './Filter'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if (state.filter === '') {
+      return state.anecdotes
+    }
+    return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter))
+  })
+
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -11,6 +18,7 @@ const AnecdoteList = () => {
 
   return (
     <div>
+      <Filter />
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
