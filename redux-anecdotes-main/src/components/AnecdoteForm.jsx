24cb24f7d3f1076d0d9385efa588c3603e2c91e1
Emilia Andersson <emilia.andersson@helsinki.fi>
2023-12-22 09:37:20 +0200

    Tehtävä 6.7

---
+++ redux-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,24 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    dispatch(createAnecdote(event.target.anecdote.value))
+    event.target[0].value = ''
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name='anecdote' /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
