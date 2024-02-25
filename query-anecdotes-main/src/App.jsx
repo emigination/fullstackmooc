01cdbe8589c6d69c962bda56b7775628e80ec96c
Emilia Andersson <emilia.andersson@helsinki.fi>
2024-02-25 14:22:59 +0200

    Tehtävä 6.20

--- query-anecdotes-main/src/App.jsx
+++ query-anecdotes-main/src/App.jsx
@@ -1,27 +1,34 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const anecdotesResult = useQuery({
+    queryKey: ['anecdotes'], queryFn: getAnecdotes
+  });
+
+  if (anecdotesResult.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (anecdotesResult.isError) {
+    return <div>Server-side error</div>;
+  }
+
+  const anecdotes = anecdotesResult.data;
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
-
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
+
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
