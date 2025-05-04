8d4d34c380f227de013116efac45dff6bc6c5f9e
Emilia Andersson <emilia.andersson@helsinki.fi>
2025-05-04 12:24:23 +0300

    exercise 9.10

--- part9/patientor/src/index.ts
+++ part9/patientor/src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
 const corsOptions: cors.CorsOptions = { origin: 'http://localhost:5173' };
 const PORT: number = 3001;
 
-app.get('/api/ping', cors(corsOptions), (_req, res) => {
+app.use(cors(corsOptions));
+
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor/src/routes/diagnoses.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { Response } from 'express';
+import { getEntries } from '../services/diagnosisService';
+import { Diagnosis } from '../types';
+
+const router = express.Router();
+
+router.get('/', (_req, res: Response<Diagnosis[]>) => {
+  const diagnoses: Diagnosis[] = getEntries();
+  res.send(diagnoses);
+});
+
+export default router;
